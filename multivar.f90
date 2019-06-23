1e62854f0d1b34df3d263ab2141ad4a5a4263f2d
Jingchang Shi <jingchangshi@gmail.com>
2019-06-23 17:46:11 +0800

    Add multi dimensional test.

--- multivar.f90
+++ multivar.f90
@@ -4,9 +4,12 @@ program original
 
   implicit none
 
-  integer, parameter :: n=3
+  integer, parameter :: n=2
   type(ADF95_dpr), dimension(1:n*n) :: fv, xv
+  type(ADF95_dpr), dimension(1:n,1:n) :: fv2, xv2
   integer :: i
+
+continue
 
   call ADF95_independent( [(i,i=1,n*n)], xv(1:n*n), real([(i,i=1,n*n)]) )
 
@@ -14,8 +17,18 @@ program original
 
   write(*,*) ADF95_value(fv)
   write(*,*) ADF95_deriv(fv,1)
-  write(*,*) ADF95_deriv(fv,2)
-  write(*,*) ADF95_deriv(fv,3)
+  write(*,*) ADF95_deriv(fv,n+1)
+
+  write(*,*) "==="
+
+  call ADF95_independent( reshape( [(i,i=1,n*n)], [n,n] ), xv2(1:n,1:n), &
+    real( reshape( [(i,i=1,n*n)], [n,n] ) ) )
+
+  fv2(1:n,1:n) = xv2(1:n,1:n) * xv2(n:1:-1,n:1:-1)
+
+  write(*,*) ADF95_value(fv2)
+  write(*,*) ADF95_deriv(fv2,1)
+  write(*,*) ADF95_deriv(fv2,n+1)
 
 end program original
 
